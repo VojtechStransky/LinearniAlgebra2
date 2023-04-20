e3d476339bd31a091a74034c40f5999c5ec407a5
Vojtech Stransky <stranskyvo@protonmail.com>
2023-04-20 12:19:58 +0200

    Comments added to gsorto

--- Solutions/gsorto.cpp
+++ Solutions/gsorto.cpp
@@ -32,13 +32,14 @@ float** createSquareMatrix(int size)
     return matrix;
 }
 
+//Method use to copy matrix member by member
 float** copyMatrix(int size, float** matrix)
 {
     float** copy = createSquareMatrix(size);
 
-    for (int i = 0; i < size; i++) //For all rows create array to represent columns
+    for (int i = 0; i < size; i++)
     {
-        for (int y = 0; y < size; y++) //For all rows create array to represent columns
+        for (int y = 0; y < size; y++)
         {
             copy[i][y] = matrix[i][y];
         }
@@ -83,6 +84,7 @@ float* askForVector(int size)
     return vector;
 }
 
+//Method used for interactive creating multiple vectors input array
 float** askForVectors(int size)
 {
     float** vectors = new float*[size];
@@ -113,7 +115,7 @@ void printMatrix(int size, float** matrix, string name = "")
     }
 }
 
-//Metod used to print a vector
+//Metod used to print vectors array
 void printVectors(int size, float** vectors, string name = "")
 {
     cout << "\nVectors \"" + name + "\":" << endl;
@@ -154,6 +156,7 @@ void changeLines(float** matrix, int index, int index2)
 }
 
 //Method calculates determinant of the inputed matrix by gauss elimination and multiplication on diagonal members
+//Then it can check linear independence or positive definition
 bool checkByDeterminant(int size, float** matrix, bool checkIndependence = false)
 {
     float result = 1;
@@ -165,7 +168,7 @@ bool checkByDeterminant(int size, float** matrix, bool checkIndependence = false
         {
             cout << ((checkIndependence) ? "Vectors are linearly dependent!" : "Myltiplication matrix is not positive definitive!") << endl;
 
-            return false; //if singular - zero
+            return false; //if singular - false
         }
 
         int index = optIndex.value();
@@ -179,7 +182,7 @@ bool checkByDeterminant(int size, float** matrix, bool checkIndependence = false
         float first = matrix[y][y]; //hold first member
         result *= first; //multiply result by another diagonal member
 
-        if (!checkIndependence && (result <= 0)) {
+        if (!checkIndependence && (result <= 0)) { //If one is negative or zero at least one of subdeterminants is negatice or zero - false
             cout << "Multiplication matrix is not positive definite!" << endl;
             return false;
         }
@@ -216,6 +219,7 @@ float** changeColumn(int size, float** matrix, float* vector, int column)
     return toReturn;
 }
 
+//Method uset to check symetricity member by member for all memberst under diagonal by comparation with complementar members, then check if positive definite
 bool checkMultiplicationMatrix(int size, float** matrix)
 {
     for (int i = 0; i < size; i++)
@@ -233,6 +237,7 @@ bool checkMultiplicationMatrix(int size, float** matrix)
     return checkByDeterminant(size, matrix);
 }
 
+//Method used to multiply vector1^T * matrix * vector2. Used for applying dot product
 float multiply(int size, float* vectorOne, float* vectorTwo, float** matrix)
 {
     int result = 0;
@@ -252,6 +257,7 @@ float multiply(int size, float* vectorOne, float* vectorTwo, float** matrix)
     return result;
 }
 
+//Method adds to first vector coef*second vector. Member by member
 float* addVectors(int size, float* vectorOne, float* vectorTwo, float vectorTwoCoefficient = 1)
 {
     float* vector = new float[size];
@@ -264,22 +270,23 @@ float* addVectors(int size, float* vectorOne, float* vectorTwo, float vectorTwoC
     return vector;
 }
 
+//Gram-Schmidt ortogonalization process
 float** makeOrtogonal(int size, float** matrix, float** vectors)
 {
-    float** result = createSquareMatrix(size);
-    float* norms = new float[size];
+    float** result = createSquareMatrix(size); //Array of arrays for result
+    float* norms = new float[size]; //helper array to hold norms to save processes number
 
-    result[0] = vectors[0];
+    result[0] = vectors[0]; //first vector of ortogonal base is first inserted
 
-    for (int i = 1; i < size; i++)
+    for (int i = 1; i < size; i++) //for all inserted vectors. Starts at second
     {
-        result[i] = vectors[i];
-        norms[i-1] = multiply(size, result[i-1], result[i-1], matrix);
+        result[i] = vectors[i]; //copy vector to results
+        norms[i-1] = multiply(size, result[i-1], result[i-1], matrix); //make norm of previous vector like preparation to be used
 
-        for (int y = 0; y < i; y++)
+        for (int y = 0; y < i; y++) //for all vectors in sum
         {
-            float coefficient =  multiply(size, vectors[i], result[y], matrix)/norms[y];
-            result[i] = addVectors(size, result[i], result[y], -coefficient);
+            float coefficient =  multiply(size, vectors[i], result[y], matrix)/norms[y]; //calculate coefficient according to formula. Dot product / norm fro array
+            result[i] = addVectors(size, result[i], result[y], -coefficient); //instead of adding in sum and then subtracting subtract each from result
         }
     }
 
@@ -295,7 +302,7 @@ int main(void)
         float** matrix = askForMatrix(size);
         printMatrix(size, matrix, "inserted matrix");
 
-        if (!checkMultiplicationMatrix(size, copyMatrix(size, matrix)))
+        if (!checkMultiplicationMatrix(size, copyMatrix(size, matrix))) //if multiplication matrix is not correct, end
         {
             return 0;
         }
@@ -303,13 +310,13 @@ int main(void)
         float** vectors = askForVectors(size);
         printVectors(size, vectors, "inserted vectors");
 
-        if (!checkByDeterminant(size, copyMatrix(size, vectors), true))
+        if (!checkByDeterminant(size, copyMatrix(size, vectors), true)) //if vectors are linearly dependent, end
         {
             return 0;
         }
 
-        float** ortogonal = makeOrtogonal(size, matrix, vectors);
-        printVectors(size, ortogonal, "ortogonal vectors");
+        float** ortogonal = makeOrtogonal(size, matrix, vectors); //create ortogonal base
+        printVectors(size, ortogonal, "ortogonal vectors"); //print result
     }
     catch (const std::exception& ex) //handle errors
     {
